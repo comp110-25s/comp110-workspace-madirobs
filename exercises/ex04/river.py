4b595c050ac3aa96fe62826860d496fa7636201d
Madison Roberts <madirobs@unc.edu>
2025-04-09 21:30:19 -0400

    final update

--- exercises/ex04/river.py
+++ exercises/ex04/river.py
@@ -20,20 +20,41 @@ class River:
         for _ in range(0, num_bears):
             self.bears.append(Bear())
 
+    def one_river_week(self):
+        for _ in range(7):
+            self.one_river_day()
+
+    def remove_fish(self, amount: int):
+        count = 0
+        while count <= amount and len(self.fish) > 0:
+            self.fish.pop(0)
+            count += 1
+
     def check_ages(self):
-        return None
+        surviving_fish = [fish for fish in self.fish if fish.age <= 3]
+        self.fish = surviving_fish
+        surviving_bears = [bear for bear in self.bears if bear.age <= 3]
+        self.bears = surviving_bears
 
     def bears_eating(self):
-        return None
+        for bear in self.bears:
+            if len(self.fish) >= 5:  # checking for atleast 5 fish
+                self.remove_fish(3)  # remove 3 fish from the river
+                bear.eat(3)  # bear eats 3 fish
 
     def check_hunger(self):
-        return None
+        surviving_bears = [bear for bear in self.bears if bear.hunger_score >= 0]
+        self.bears = surviving_bears
 
     def repopulate_fish(self):
-        return None
+        new_fish = (len(self.fish) // 2) * 4  # each pair of fish produces 4 offspring
+        for _ in range(new_fish):
+            self.fish.append(Fish())  # append each new fish one by one
 
     def repopulate_bears(self):
-        return None
+        new_bears = len(self.bears) // 2  # each pair of bears produces 1 cub
+        for _ in range(new_bears):
+            self.bears.append(Bear())  # append each new bear one by one
 
     def view_river(self):
         print(f"~~~ Day {self.day}: ~~~")
